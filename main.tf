1026b06c75972ed7e4752453ad22d26562277344
EC2 Default User <ec2-user@ip-172-31-44-236.ec2.internal>
2023-09-25 08:32:12 +0000

    init code

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ data "aws_ami" "ami" {
 
 resource "aws_instance" "ansible_server" {
   ami           = data.aws_ami.ami.id
-  instance_type = "t3.small"
+  instance_type = "t2.small"
 
   lifecycle {
     create_before_destroy = true
